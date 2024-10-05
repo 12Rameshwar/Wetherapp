cedacecd228113c17a8ee40af6ec9ec97416f8b2
12Rameshwar <chavhanrameshwar13@gmail.com>
2024-10-05 18:49:47 +0530

    its wether app

---
+++ src/components/Credit.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Credit() {
+  return (
+    <div class="credit">
+    <p>
+      Coded by
+      <a > Rameshwar Chavhan</a
+      >, <a ></a>Best of luck for your journy 
+    </p>
+  </div>
+  );
+}
